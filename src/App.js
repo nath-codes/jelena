58638e6e22f04d35541b88a2e6670b52ac10067f
Nathan Tredgett <nathantredgett@gmail.com>
2018-05-14 21:37:28 +0100

    added basic global styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,36 @@
 import React, { Component } from "react";
 import { ThemeProvider } from "styled-components";
+import { injectGlobal } from "styled-components";
 import theme from "./constants/theme";
+import Heading from "./components/Heading";
+
+injectGlobal`
+  html {
+    box-sizing: border-box;
+  }
+
+  *,
+  *::before,
+  *::after {
+    box-sizing: inherit;
+  }
+
+  body {
+    color: '#333';
+    font-family: 'montserrat';
+    font-size: 16px;
+    line-height: 1.5;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  img {
+    height: auto;
+    max-width: 100%;
+  }
+`;
 
 class App extends Component {
   render() {
