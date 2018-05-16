65b2266bd6b7f99b7466dfcde8cbebbce228165e
Nathan Tredgett <nathantredgett@gmail.com>
2018-05-16 21:35:02 +0100

    added basic Gifts component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Hero from "./components/Hero";
 import Rsvp from "./components/Rsvp";
 import Timetable from "./components/Timetable";
 import Directions from "./components/Directions";
+import Gifts from "./components/Gifts";
 
 injectGlobal`
   html {
@@ -43,6 +44,7 @@ class App extends Component {
           <Rsvp />
           <Timetable />
           <Directions />
+          <Gifts />
         </div>
       </ThemeProvider>
     );
---
+++ src/components/Gifts.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Heading from "./Section";
+import Section from "./Section";
+import Text from "./Text";
+
+const Hero = () => (
+  <Section>
+    <Heading>Gifts</Heading>
+    <Text>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam minima
+      deserunt dolore animi itaque nemo doloremque? Aut, aperiam in quam commodi
+      corrupti laborum assumenda, dolorum impedit exercitationem nesciunt atque
+      vel!
+    </Text>
+  </Section>
+);
+
+export default Hero;
