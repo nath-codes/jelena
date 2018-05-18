0563796c1d100c8e428f98bf46da7aa56fba8151
Nathan Tredgett <nathantredgett@gmail.com>
2018-05-18 14:20:46 +0100

    added ButtonLink component to Gift component

--- src/components/Gifts.jsx
+++ src/components/Gifts.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Heading from "./Heading";
 import Section from "./Section";
 import Text from "./Text";
+import ButtonLink from "./ButtonLink";
 
 const Hero = () => (
   <Section>
@@ -12,6 +13,7 @@ const Hero = () => (
       corrupti laborum assumenda, dolorum impedit exercitationem nesciunt atque
       vel!
     </Text>
+    <ButtonLink href="https://amazon.co.uk">Ipsum</ButtonLink>
   </Section>
 );
 
