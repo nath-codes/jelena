afeb1a746d1396dff54c8e4e08721d5244ed2ebd
Nathan Tredgett <nathantredgett@gmail.com>
2018-05-20 13:14:50 +0100

    adjusted Heading component to use spacer mixin

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -1,12 +1,13 @@
 import styled from "styled-components";
-import { modularScale } from "polished";
+import spacer from "../mixins/spacer";
 
 const StyledHeading = styled.h2`
   color: ${props => props.theme.colors.delftBlue};
   font-family: ${props => props.theme.fonts.heading};
-  font-size: ${modularScale(5)};
+  font-size: ${spacer(3)};
   font-weight: 400;
-  line-height: 1;
+  line-height: ${spacer(3)};
+  margin: 0 0 ${spacer(2)};
   text-align: center;
 `;
 
