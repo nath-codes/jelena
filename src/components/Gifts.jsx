061ab78829f8de536e6a04acd6b5fa14f9a518a4
Nathan Tredgett <nathantredgett@gmail.com>
2018-05-20 13:10:11 +0100

    added bottom margin to Gifts Text component

--- src/components/Gifts.jsx
+++ src/components/Gifts.jsx
@@ -3,16 +3,21 @@ import Heading from "./Heading";
 import Section from "./Section";
 import Text from "./Text";
 import ButtonLink from "./ButtonLink";
+import spacer from "../mixins/spacer";
+
+const GiftsText = Text.extend`
+  margin-bottom: ${spacer(2)};
+`;
 
 const Hero = () => (
   <Section>
     <Heading>Gifts</Heading>
-    <Text>
+    <GiftsText>
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam minima
       deserunt dolore animi itaque nemo doloremque? Aut, aperiam in quam commodi
       corrupti laborum assumenda, dolorum impedit exercitationem nesciunt atque
       vel!
-    </Text>
+    </GiftsText>
     <ButtonLink href="https://amazon.co.uk">Ipsum</ButtonLink>
   </Section>
 );
