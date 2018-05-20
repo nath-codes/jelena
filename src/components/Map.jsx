227b0643b53d42d496d20ed6837c0efa8660af65
Nathan Tredgett <nathantredgett@gmail.com>
2018-05-20 13:11:37 +0100

    increased height of Map component as it was too small

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,18 +1,20 @@
 import React from "react";
 import styled from "styled-components";
+import spacer from "../mixins/spacer";
 
 const placeId = "ChIJW9LhQoWScUgRpUZbE5MC6tY";
 const StyledIframe = styled.iframe`
   border: 0;
   filter: grayscale(1);
+  height: ${spacer(13)};
+  margin-bottom: ${spacer(2)};
+  width: 100%;
 `;
 
 const src = `https://www.google.com/maps/embed/v1/place?key=${
   process.env.REACT_APP_GOOGLE_MAPS_API_KEY
 }&q=place_id:${placeId}`;
 
-const Map = () => (
-  <StyledIframe width="100%" height="auto" frameborder="0" src={src} />
-);
+const Map = () => <StyledIframe frameborder="0" src={src} />;
 
 export default Map;
