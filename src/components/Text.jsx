df4441af0b1992dec3a1f475dd785392025fbd54
Nathan Tredgett <nathantredgett@gmail.com>
2018-05-21 21:05:05 +0100

    added size logic to Text component

--- src/components/Text.jsx
+++ src/components/Text.jsx
@@ -1,9 +1,13 @@
 import styled from "styled-components";
+import spacer from "../mixins/spacer";
+import { modularScale } from "polished";
 
-const Copy = styled.div`
+const Text = styled.div`
   align-self: center;
-  line-height: 1.5625;
-  width: 50vw;
+  font-size: ${props =>
+    props.size === "large" ? modularScale(1) : modularScale(0)};
+  line-height: 1.5;
+  max-width: ${spacer(22)};
 `;
 
-export default Copy;
+export default Text;
