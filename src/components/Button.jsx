c1e6325fda2d2eb16fbc0f46c592f14d821acdfb
Nathan Tredgett <nathantredgett@gmail.com>
2018-05-24 21:38:34 +0100

    amended button so it looks less less a text input

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,15 +3,18 @@ import styled from "styled-components";
 
 const Button = styled.button`
   align-self: center;
-  border: solid 3px ${props => props.theme.colors.delftBlue};
-  background: none;
-  color: ${props => props.theme.colors.delftBlue};
+  background: ${props => props.theme.colors.delftBlue};
+  border: 0;
+  color: ${props => props.theme.colors.white};
   cursor: pointer;
-  padding: 13px ${spacer(2)};
+  font-family: ${props => props.theme.fonts.heading};
+  font-size: ${spacer(1)};
+  line-height: 1;
+  padding: ${spacer(0.5)} ${spacer(2)};
   transition: all ${props => props.theme.transition.duration};
 
   &:hover {
-    background: ${props => props.theme.colors.delftBlue};
+    background: ${props => props.theme.colors.black};
     color: ${props => props.theme.colors.white};
   }
 `;
