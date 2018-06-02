999ba4a616e668ff3dd5985330ce18adb5f57e59
Nathan Tredgett <nathantredgett@gmail.com>
2018-06-02 13:18:38 +0100

    added 'active' state to Button component which will display a spinner

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,5 +1,6 @@
 import spacer from "../mixins/spacer";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
+import spinner from "../assets/img/spinner.svg";
 
 const Button = styled.button`
   align-self: center;
@@ -11,12 +12,30 @@ const Button = styled.button`
   font-size: ${spacer(1)};
   line-height: 1;
   padding: ${spacer(0.5)} ${spacer(2)};
+  position: relative;
   transition: all ${props => props.theme.transition.duration};
 
   &:hover {
     background: ${props => props.theme.colors.black};
     color: ${props => props.theme.colors.white};
   }
+
+  ${props =>
+    props.active &&
+    css`
+      padding-right: ${spacer(3)};
+
+      &:after {
+        background-image: url(${spinner});
+        background-size: ${spacer(1)} ${spacer(1)};
+        content: " ";
+        height: ${spacer(1)};
+        position: absolute;
+        right: ${spacer(1)};
+        top: ${spacer(0.5)};
+        width: ${spacer(1)};
+      }
+    `};
 `;
 
 export default Button;
