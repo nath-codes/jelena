6590cd4ecd51a2f1b53084622d8785b8283e9a9e
Nathan Tredgett <nathantredgett@gmail.com>
2018-06-02 13:19:15 +0100

    added service to send RSVP email

---
+++ src/services/send-email.js
@@ -0,0 +1,49 @@
+import axios from "axios";
+
+const generateEmailText = fields => {
+  const {
+    attending,
+    dessert,
+    dietaryRequirements,
+    fullName,
+    main,
+    starter
+  } = fields;
+
+  return `
+  Hello there,
+
+  ${fullName} has just submitted an RSVP to your wedding.
+
+  Attending: ${attending}.
+  ${attending === "Yes" &&
+    `
+  Starter: ${starter}.
+  Main: ${main}.
+  Dessert: ${dessert}.
+  ${dietaryRequirements && `Dietary Requirements: ${dietaryRequirements}.`}
+`}
+
+Have a great day.
+
+The RSVP Bot
+
+(°◡°♡).:｡`;
+};
+
+const sendEmail = fields => {
+  const text = generateEmailText(fields);
+
+  return axios({
+    method: "POST",
+    url: process.env.REACT_APP_MAIL_API_URL,
+    data: {
+      from: `The RSVP Bot <rsvp@${process.env.REACT_APP_MAILGUN_DOMAIN}>`,
+      to: process.env.REACT_APP_RSVP_EMAIL,
+      subject: "You have recieved an RSVP to your wedding",
+      text
+    }
+  });
+};
+
+export default sendEmail;
