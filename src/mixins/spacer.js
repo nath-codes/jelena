e0f65efd74b49ffbf2aee2fb0dd380cf5f4a9a70
Nathan Tredgett <nathantredgett@gmail.com>
2018-06-05 07:56:37 +0100

    refactored spacer mixin to allow for small and medium devices

--- src/mixins/spacer.js
+++ src/mixins/spacer.js
@@ -1,8 +1,20 @@
 import { rem } from "polished";
 
-const height = 24;
+const getLineHeight = breakpoint => {
+  switch (breakpoint) {
+    case "small":
+      return 1.3;
+    case "medium":
+      return 1.4;
+    default:
+      return 1.5;
+  }
+};
 
-export default (increment = 1) => {
-  const space = increment * height;
-  return rem(space);
+export default (increment = 1, breakpoint) => {
+  const baseFontSize = 16;
+  const lineHeight = getLineHeight(breakpoint);
+  const spacer = increment * (baseFontSize * lineHeight);
+
+  return rem(spacer);
 };
