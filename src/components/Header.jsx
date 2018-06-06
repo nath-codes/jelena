52f5da5cf03d6e4d9e33245c767be9f333a693ff
Nathan Tredgett <nathantredgett@gmail.com>
2018-06-06 17:51:14 +0100

    added additional margin to header on small devices

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,11 +2,16 @@ import React from "react";
 import Nav from "./Nav";
 import styled from "styled-components";
 import spacer from "../mixins/spacer";
+import { small } from "../constants/breakpoints";
 
 const StyledHeader = styled.header`
   display: flex;
   justify-content: center;
-  margin: ${props => spacer(1, props.breakpoint)} 0 0;
+  margin: ${props => spacer(2, props.breakpoint)} 0 0;
+
+  @media (min-width: ${small}) {
+    margin-top: ${props => spacer(1, props.breakpoint)} 0 0;
+  }
 `;
 
 const Header = () => (
