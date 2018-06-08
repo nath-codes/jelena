815f945d425a715a6983e62e69de27eff22e800e
Nathan Tredgett <nathantredgett@gmail.com>
2018-06-08 13:14:58 +0100

    added window.matchMedia mock to App.test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
+const matchMediaMock = () => {
+  return {
+    matches: false,
+    addListener: function() {},
+    removeListener: function() {}
+  };
+};
+
+window.matchMedia = window.matchMedia || matchMediaMock;
+
+it("renders without crashing", () => {
+  const div = document.createElement("div");
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
