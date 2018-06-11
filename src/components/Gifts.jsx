9a840000d70df89ae4ef8d6646ef75bf20ad6929
Nathan Tredgett <nathantredgett@gmail.com>
2018-06-11 20:43:04 +0100

    added correct copy to Gift component

--- src/components/Gifts.jsx
+++ src/components/Gifts.jsx
@@ -3,6 +3,8 @@ import Heading from "./Heading";
 import Section from "./Section";
 import Text from "./Text";
 import ButtonLink from "./ButtonLink";
+import Link from "./Link";
+import Paragraph from "./Paragraph";
 import spacer from "../mixins/spacer";
 
 const GiftsText = Text.extend`
@@ -13,12 +15,27 @@ const Hero = () => (
   <Section id="gifts">
     <Heading>Gifts</Heading>
     <GiftsText>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam minima
-      deserunt dolore animi itaque nemo doloremque? Aut, aperiam in quam commodi
-      corrupti laborum assumenda, dolorum impedit exercitationem nesciunt atque
-      vel!
+      <Paragraph>
+        Simply by being at our wedding, you will be getting us a wonderful gift
+        – most of all, we want to spend the day with everyone we love and for
+        you to celebrate with us.
+      </Paragraph>
+
+      <Paragraph>
+        Should you want to give us anything extra, however, we will be going to
+        the beautiful archipelago of the Azores on our honeymoon and would
+        really appreciate any contribution to help us on our adventures.
+      </Paragraph>
+
+      <Paragraph>
+        If you would like to make a gift to our honeymoon fund you can do so via
+        PayPal using the button below or by calling our travel agent, Azores
+        Choice, on <Link href="tel:01768 721020">01768 721020</Link>
+      </Paragraph>
     </GiftsText>
-    <ButtonLink href="https://amazon.co.uk">Contribute</ButtonLink>
+    <ButtonLink href="https://www.paypal.com/pools/c/858WNidwXt">
+      Contribute
+    </ButtonLink>
   </Section>
 );
 
