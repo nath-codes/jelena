ea39cd39a5f0e2e284e4f9fa91fc96fc8069aefc
Nathan Tredgett <nathantredgett@gmail.com>
2018-06-13 14:26:05 +0100

    removed trailing margin in Paragraph component

--- src/components/Paragraph.jsx
+++ src/components/Paragraph.jsx
@@ -3,5 +3,9 @@ import spacer from "../mixins/spacer";
 
 const Paragraph = styled.p`
   margin: 0 0 ${props => spacer(1, props.theme.breakpoint)};
+
+  &:last-child {
+    margin-bottom: 0;
+  }
 `;
 export default Paragraph;
