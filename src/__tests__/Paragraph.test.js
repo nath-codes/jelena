461540a3fbe944c3a9abcefd13aea9aaea8f7e29
Nathan Tredgett <nathantredgett@gmail.com>
2018-08-15 15:06:26 +0100

    added Paragraph test and snapshot

---
+++ src/__tests__/Paragraph.test.js
@@ -0,0 +1,12 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import "jest-styled-components";
+import Paragraph from "../components/Paragraph";
+import theme from "../constants/theme";
+
+describe("<Paragraph />", () => {
+  it("renders correctly", () => {
+    const tree = renderer.create(<Paragraph theme={theme} />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
