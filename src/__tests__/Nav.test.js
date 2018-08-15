9b2b54c2c4915a234f77949e7a30a0b24609abef
Nathan Tredgett <nathantredgett@gmail.com>
2018-08-15 15:40:24 +0100

    added Nav test and snapshot

---
+++ src/__tests__/Nav.test.js
@@ -0,0 +1,19 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import "jest-styled-components";
+import { ThemeProvider } from "styled-components";
+import Nav from "../components/Nav";
+import theme from "../constants/theme";
+
+describe("<Nav />", () => {
+  it("renders correctly", () => {
+    const tree = renderer
+      .create(
+        <ThemeProvider theme={theme}>
+          <Nav />
+        </ThemeProvider>
+      )
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
