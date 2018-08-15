567f7ee64fcf6ffd5cc57aeb387f32233759dcf3
Nathan Tredgett <nathantredgett@gmail.com>
2018-08-15 15:35:18 +0100

    added Location component

---
+++ src/__tests__/Location.test.js
@@ -0,0 +1,19 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import "jest-styled-components";
+import { ThemeProvider } from "styled-components";
+import Location from "../components/Location";
+import theme from "../constants/theme";
+
+describe.only("<Location />", () => {
+  it("renders correctly", () => {
+    const tree = renderer
+      .create(
+        <ThemeProvider theme={theme}>
+          <Location />
+        </ThemeProvider>
+      )
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
