75827415cdacfe83e9813b596519408192259f71
Nathan Tredgett <nathantredgett@gmail.com>
2018-08-15 15:53:25 +0100

    added Header test and snapshot

---
+++ src/__tests__/Header.test.js
@@ -0,0 +1,19 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import "jest-styled-components";
+import { ThemeProvider } from "styled-components";
+import Header from "../components/Header";
+import theme from "../constants/theme";
+
+describe("<Header />", () => {
+  it("renders correctly", () => {
+    const tree = renderer
+      .create(
+        <ThemeProvider theme={theme}>
+          <Header />
+        </ThemeProvider>
+      )
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
