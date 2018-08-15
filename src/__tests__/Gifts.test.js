ff7fda7c08ae9964ead83a751bff6c26f1b919f5
Nathan Tredgett <nathantredgett@gmail.com>
2018-08-15 15:54:36 +0100

    added Gifts test and snapshot

---
+++ src/__tests__/Gifts.test.js
@@ -0,0 +1,19 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import "jest-styled-components";
+import { ThemeProvider } from "styled-components";
+import Gifts from "../components/Gifts";
+import theme from "../constants/theme";
+
+describe("<Gifts />", () => {
+  it("renders correctly", () => {
+    const tree = renderer
+      .create(
+        <ThemeProvider theme={theme}>
+          <Gifts />
+        </ThemeProvider>
+      )
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
