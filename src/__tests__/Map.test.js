bfdca6717faf1b4f77a14c34107bd4f12fbaf705
Nathan Tredgett <nathantredgett@gmail.com>
2018-08-15 15:25:04 +0100

    added Map test and snapshot

---
+++ src/__tests__/Map.test.js
@@ -0,0 +1,17 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import "jest-styled-components";
+import Map from "../components/Map";
+import theme from "../constants/theme";
+
+describe("<Map />", () => {
+  it("renders correctly", () => {
+    const tree = renderer.create(<Map theme={theme} />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it("renders active state", () => {
+    const tree = renderer.create(<Map theme={theme} active={true} />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
